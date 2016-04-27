a15803a97b661fa89993a79a042fd1eca88147c2
Tom Peck <tapeck@umass.edu>
2016-04-27 12:53:30 -0400

    its working

--- Factor.java
+++ Factor.java
@@ -22,36 +22,16 @@ public class Factor {
   }
   public static ArrayList<Integer> recursiveFactor(ArrayList<Integer> fac){
     if(factored(fac)) return fac;
-    for(int i: fac){
-      if(!isPrime(i)) {
+    for(int i=0; i<fac.size();i++){
+      if(!isPrime(fac.get(i))) {
         int[] f = factors(fac.get(i));
         fac.remove(i);
         fac.add(f[0]);
         fac.add(f[1]);
+        break;
       }
     }
     return recursiveFactor(fac);
-  }
-  public static ArrayList<Integer> primeFactorization(int x){
-    ArrayList<Integer> fac = new ArrayList<Integer>();
-    fac.add(x);
-    if(isPrime(x)) return fac; //already factored!
-    while(!factored(fac)){
-      for(int i=2;i<x;i++){
-        if(fac.get(fac.size()-1) % i == 0) {
-          int y = fac.remove(fac.size()-1);
-          fac.add(i);
-          fac.add(y/i);
-        }
-      }
-    }
-    for(int i=0;i<fac.size();i++){
-      if(fac.get(i) == 1){
-        fac.remove(i);
-        --i;
-      }
-    }
-    return fac;
   }
   public static void main(String[] args) {
     if(args.length == 0){
